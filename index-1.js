8bbf7eb9a2ce5de98f0ffbeb3fdfa6e861537b1e
Mykhailo Den <95014048+MikleDen@users.noreply.github.com>
2023-01-17 15:23:17 +0200

    updt

--- index-1.js
+++ index-1.js
@@ -1,107 +1,132 @@
-// 03-module-01-lesson
+// 03-module-02-lesson
+'use strict';
+
 /*
- * Створення об'єкта (конструктор, літерал).
- * Властивості, ключі (ім'я) та значення
+ * Операція spread як заміна concat та slice
  */
 
-// Сутність фільм (назва, опис, рейтинг, акторський склад, вікове обмеження)
-const film = {
-    title: 'Matrix',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, nemo iusto! Et doloribus praesentium laboriosam dolorem veritatis, magnam atque amet!',
-    rating: 4,
-    actors: ['Aiden', 'Mason', 'Jimmy'],
-    isAdult: true,
-    location: {
-      country: 'Jamaica',
-      city: 'Ocho Rios',
-    },
-  };
-  // console.log(film.title);
-  // film.title = 'Matrix2';
-  // console.log(film.actors);
-  // film.actors.push('Obivan');
-  // console.log(film.actors);
-  /*
-   * Доступ до властивостей(obj.key, obj[key]).
-   * Додавання та зміна властивостей
-   */
-  
-  // film['preview'] = './images/img.jpg';
-  // film.preview = './images/img.jpg';
-  
-  // film.isAdult = false;
-  // film['isAdult] = false;
-  
-  /*
-   *Короткий запис властивостей
-   */
-  
-  // const username = 'Oleksii';
-  // const email = 'test@gmail.com';
-  
-  // const signupData = {
-  //   username,
-  //   email,
-  // };
-  // const signupData = {
-  //   username: username,
-  //   email: email,
-  // };
-  
-  // console.log(signupData);
-  
-  // const firstName = prompt("Введіть ваше ім'я");
-  // const email = prompt('Ваш email');
-  
-  // const user = {
-  //   firstName,
-  //   email,
-  // };
-  
-  // console.log(user);
-  // const f = {};
-  // const d = {};
-  // const a = {
-  //   b: 1,
-  //   c: 2,
-  // };
-  // a[f] = 3;
-  // a[d] = 4;
-  // console.log(a);
-  /*
-   * Посилальний тип
-   */
-  
-  // const obj = {
-  //   a: 1,
-  //   b: 2,
-  // };
-  
-  // const obj2 = obj;
-  
-  // console.log('obj: ', obj);
-  // console.log('obj2: ', obj2);
-  // console.log(obj === obj2);
-  
-  // obj2.c = 5;
-  
-  // console.log('obj: ', obj);
-  // console.log('obj2: ', obj2);
-  // console.log(obj === obj2);
-  
-  /*
-   *Все у цьому світі об'єкти
-   */
-  // const arr = [1, 2, 3, 4];
-  
-  // const obj = {
-  //   0: 1,
-  //   1: 2,
-  //   2: 3,
-  //   3: 4,
-  //   length: 4,
-  // };
-  
-  // console.log(Array.isArray(arr));
-  // console.log(Array.isArray(obj));
+//*  Копія масива
+// const numbers = [1, 2, 3, 4, 5];
+// const numbers2 = numbers.slice();
+
+// const numbers2 = [...numbers];
+// console.log(numbers2);
+
+// console.log('numbers: ', numbers);
+// console.log('numbers2: ', numbers2);
+// console.log(numbers === numbers2);
+
+// numbers2[0] = 10;
+
+// console.log('numbers: ', numbers);
+// console.log('numbers2: ', numbers2);
+
+//* Об'єднання масивів
+// const numbers = [1, 2, 3, 4, 5];
+// const numbers2 = [10, 9, 8, 7];
+
+// const allNumbers = numbers.concat(numbers2);
+
+// const allNumbers = [...numbers, ...numbers2];
+
+// console.log(allNumbers);
+
+//* Розпилення масиву у функцію
+// const numbers = [1, 2, 3, 4, 5];
+
+// console.log(Math.max(...numbers));
+// console.log(Math.min(...numbers));
+
+/*
+ * Операція spread як заміна Object.assign({}, ...sources)
+ */
+
+// const o1 = { a: 1 };
+// const o2 = { b: 2 };
+// const o3 = { c: 3 };
+
+// const obj = Object.assign(o1, o2, o3);
+// console.log(obj); // { a: 1, b: 2, c: 3 }
+// console.log(o1); // { a: 1, b: 2, c: 3 }, изменился и сам целевой объект.
+
+// const animal = {
+//   legs: 4,
+// };
+
+// const cat = Object.create(animal);
+// cat.name = 'Kiwi';
+// console.log(cat);
+// const newCat = _.cloneDeep(cat);
+// const newCat = Object.assign(cat);
+// const newCat2 = { ...cat };
+// console.log(newCat);
+// console.log(newCat2);
+
+// const user = {
+//   firstName: 'Jonathan',
+//   lastName: 'Barnett',
+//   age: 30,
+// };
+
+// const user2 = { ...user };
+
+// console.log('user: ', user);
+// console.log('user2: ', user2);
+// // console.log(user === user2);
+
+// user2.firstName = 'Birdie';
+// console.log('---------------------');
+// console.log('user: ', user);
+// console.log('user2: ', user2);
+
+//* Розпилення складних типів
+
+// const user = {
+//   firstName: 'Jonathan',
+//   lastName: 'Barnett',
+//   age: 30,
+//   someArr: [1, 2, 3, 4],
+// };
+
+// const user2 = _.cloneDeep(user);
+// const user2 = { ...user };
+// console.log('user: ', user);
+// console.log('user2: ', user2);
+// console.log(user === user2);
+// user2.someArr.push(10);
+
+// console.log('user: ', user);
+// console.log('user2: ', user2);
+
+/*
+ * Операція rest
+ */
+
+//? Напишіть функцію sum, яка додає довільну кількість аргументів які більше вказаного числа
+// const sum = function (num, ...args) {
+//   //   console.log(num);
+//   //   console.log(args);
+//   let total = 0;
+//   for (const item of args) {
+//     // if (item > num) {
+//     // console.log(typeof item !== 'number' || Number.isNaN(item));
+//     // // console.log(typeof item);
+//     // console.log(item);
+//     // console.log('------------');
+//     if (typeof item !== 'number' || Number.isNaN(item)) {
+//       continue;
+//     }
+//     // console.log(Number.isNaN(item));
+
+//     // console.log(typeof item);
+//     total += item;
+//     // }
+//   }
+//   console.log(total);
+// };
+
+// const numbers = [1, 2, 3, 4];
+// sum(5, 6, 4, NaN, 14, 85, 2, 3, 'df', 'df', 'dfs', NaN);
+// console.log(sum(...numbers));
+// console.log(sum(10, 20, 30));
+// console.log(typeof NaN);
--- index-3.js
+++ index-3.js
@@ -1,126 +1,33 @@
-/*
- * for...in, Object.keys, Object.values
- */
+'use strict';
 
-//? Отримайте суму зарплат усіх працівників
+const logFunction = function ({
+  firstName,
+  lastName,
+  age,
+  gender,
+  userEmail: mail = 'usermail@gmail.com',
+  userPhoneNumber: number = '444 555 666',
+}) {
+  // console.log(userInfo);
 
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
+  // const { firstName, lastName, age, gender, userEmail, userPhoneNumber } =
+  //   userInfo;
+  console.log(firstName);
+  console.log(lastName);
+  console.log(age);
+  console.log(gender);
+  console.log(mail);
+  console.log(number);
+  // const { firstName, lastName, age, gender, userEmail, userPhoneNumber } =
+  //   userInfo;
+  // console.log(firstName, lastName, age, gender, userEmail, userPhoneNumber);
+};
 
-//TODO: Через for...in
-
-// let total = 0;
-
-// const arr = [1, 2, 9, 6, 8];
-
-// for (const fasdgasfg of arr) {
-//   console.log(fasdgasfg);
-// }
-
-// for (const key in salaries) {
-//   // console.log(key);
-//   // console.log(salaries[key]);
-//   total += salaries[key];
-// }
-// console.log(total);
-
-// console.log(total);
-
-//TODO: Через Object.keys();
-// const salariesKeys = Object.keys(salaries);
-// console.log(salariesKeys);
-// // // let total = 0;
-
-// for (const key of salariesKeys) {
-//   total += salaries[key];
-// }
-
-// console.log(total);
-
-//TODO: Через Object.values();
-
-// const salariesValues = Object.values(salaries);
-// console.log(salariesValues);
-// let total = 0;
-
-// for (const value of salariesValues) {
-//   total += value;
-// }
-
-// console.log(total);
-
-// const obj = {
-//   a: 1,
-//   b: 2,
-// };
-
-// const obj1 = Object.create(obj);
-// obj1.c = 10;
-
-// console.log(obj1);
-
-// for (const key in obj1) {
-//   if (obj1.hasOwnProperty(key)) {
-//     console.log(key);
-//   }
-// }
-
-// const animal = {
-//   legs: 4,
-// };
-// const dog = Object.create(animal);
-
-// const cat = Object.create(animal);
-// dog.name = 'Kiwi';
-
-// console.log(dog.hasOwnProperty('legs'));
-
-const salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130,
-  };
-  const developersSalaaries = Object.create(salaries);
-  let total = 0;
-  developersSalaaries.Obivan = 200;
-  developersSalaaries.Yoda = 500;
-  developersSalaaries.Chybaka = 50;
-  developersSalaaries.DartVaider = 250;
-  // console.log(developersSalaaries);
-  // for (const key in developersSalaaries) {
-  //   // if (developersSalaaries.hasOwnProperty(key)) {
-  //   //   console.log(key, developersSalaaries.hasOwnProperty(key));
-  //   //   total += developersSalaaries[key];
-  //   // }
-  //   // console.log(key, developersSalaaries.hasOwnProperty(key));
-  //   if (!developersSalaaries.hasOwnProperty(key)) {
-  //     // console.log(key, developersSalaaries.hasOwnProperty(key));
-  //     continue;
-  //   }
-  //   console.log(key, developersSalaaries.hasOwnProperty(key));
-  //   total += developersSalaaries[key];
-  // }
-  // const developersSalaariesValues = Object.values(developersSalaaries);
-  // for (const qwe of developersSalaariesValues) {
-  //   total += qwe;
-  // }
-  // console.log(total);
-  
-  // const developers = Object.keys(developersSalaaries);
-  // // console.log(developers);
-  // for (const name of developers) {
-  //   // console.log(developersSalaaries[name]);
-  
-  //   total += developersSalaaries[name];
-  // }
-  
-  // console.log(total);
-  
-  const developersEntries = Object.entries(developersSalaaries);
-  // console.log(developersEntries);
-  for (const item of developersEntries) {
-    console.log(item);
-  }
+logFunction({
+  firstName: 'Amelia',
+  lastName: 'Burgess',
+  age: 30,
+  gender: 'female',
+  // userEmail: 'apjez@eh.aq',
+  // userPhoneNumber: '(096) 35-21-476',
+});
